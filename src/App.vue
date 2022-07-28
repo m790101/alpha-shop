da4669e50bce3cd9b90dc6538c5be82b92460aed
m790101 <m790101@hotmail.com>
2022-07-28 12:06:49 +0800

    initial project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderNav/>
+    <template>
+    </template>
+   
   </div>
 </template>
 
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNav
   }
 }
 </script>
@@ -21,8 +26,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+  <ul class="nav-list-375">
+      <li class="nav-item"><a href="#">男款</a></li>
+      <li class="nav-item"><a href="#">女款</a></li>
+      <li class="nav-item"><a href="#">最新消息</a></li>
+      <li class="nav-item"><a href="#">客製商品</a></li>
+    <li class="nav-item"><a href="#">聯絡我們</a></li>
+    </ul>
+  <div class="logo logo-nav">
+    <img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/14985/logo-2021.png" class="logo-image d-block">
+    <p class="font-heavy">ALPHA Shop</p>
+  </div>
+ 
+  <div class="function-icon d-none">
+    <i class="fas fa-search"></i>
+    <i class="fas fa-shopping-cart"></i>
+      <i class="far fa-moon"></i>
+  </div>
+  
+</header>
+</template>
+
+
